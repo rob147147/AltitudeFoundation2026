302c22267f852985a5127d7ba7d50974f34b76cb
Rob Powell <rob147147@hotmail.com>
2026-08-11 09:05:47 +0100

    Update Task3.py
    Update to camera 1

--- Task3.py
+++ Task3.py
@@ -11,7 +11,7 @@ import numpy as np
 
 # define video capture with access to camera 0
 
-camera = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+camera = cv2.VideoCapture(1, cv2.CAP_DSHOW)
 
 # define display window
 
